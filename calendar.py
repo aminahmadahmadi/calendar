9fc5767f8c1883fa59b47275f7d0e89a01a49bd8
aminahamadahmadi <aminahmadahmadi@gmail.com>
2022-02-22 12:21:32 +0330

    clandar class init

---
+++ calendar.py
@@ -0,0 +1,11 @@
+﻿from pages import *
+from notebook import *
+
+
+class Calendar(Notebook):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+    def addWeekPage(self):
+        page = WeekPage()
+        self.pages.append(page)
