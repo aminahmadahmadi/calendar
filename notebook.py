9575273db6fdb40269a043b6adb2b9fb54a78e35
aminahamadahmadi <aminahmadahmadi@gmail.com>
2022-02-23 19:47:53 +0330

    get margin and padding in list and dict style / rewrite toHTML func

--- notebook.py
+++ notebook.py
@@ -1,4 +1,5 @@
 ﻿from pages import *
+import os
 
 
 class Notebook():
@@ -11,12 +12,21 @@ class Notebook():
         self.scale = kwargs.get('scale', 2.83465)
 
         # margin and padding
-        nameList = ['top', 'outside', 'bottom', 'inside']
-        margin = kwargs.get('margin', [5, 5, 5, 0])
-        padding = kwargs.get('padding', [32, 0, 0, 0])
+        self.margin = kwargs.get(
+            'margin',
+            {'top': 5, 'outside': 5, 'bottom': 5, 'inside': 0}
+        )
+        if isinstance(self.margin, list):
+            lst = ['top', 'outside', 'bottom', 'inside']
+            self.margin = dict(zip(lst, self.margin))
 
-        self.margin = dict(zip(nameList, margin))
-        self.padding = dict(zip(nameList, padding))
+        self.padding = kwargs.get(
+            'padding',
+            {'top': 32, 'outside': 0, 'bottom': 0, 'inside': 0}
+        )
+        if isinstance(self.padding, list):
+            lst = ['top', 'outside', 'bottom', 'inside']
+            self.padding = dict(zip(lst, self.padding))
 
         # line and dot peroperty
         self.lineHeight = kwargs.get('lineHeight', 6)
@@ -24,12 +34,13 @@ class Notebook():
 
         # colors
         self.bgColor = kwargs.get('bgColor', 'none')
-        self.lineColor = kwargs.get('lineColor', '#000')
+        self.lineColor = kwargs.get('lineColor', '#555')
 
         # pages
         self.pages = kwargs.get('pages', [])
 
-        # last page location
+        # info on page
+        self.guide = kwargs.get('guide', False)
 
     def addEmptyPage(self):
         page = Page(**self.__dict__)
@@ -44,12 +55,34 @@ class Notebook():
         self.pages.append(page)
 
     def toHTML(self, Dir=''):
-        htmlTxt = '<html>\n<head>\n<style>\nhtml,body{margin:0;padding: 0;}\n</style>\n</head>\n<body>'
-        for page in self.pages:
-            htmlTxt += f'{page.svgText}\n'
+        if not os.path.exists('\\'.join([Dir, self.name])):
+            os.mkdir('\\'.join([Dir, self.name]))
+
+        if not os.path.exists('\\'.join([Dir, self.name, 'pages'])):
+            os.mkdir('\\'.join([Dir, self.name, 'pages']))
+
+        htmlTxt = '<html>\n<head>\n<style>\nhtml,body{margin:0;padding: 0;}\n</style>\n</head>\n<body>\n'
+        for i in range(len(self.pages)):
+            if i % 2 == 0 ^ self.rtl:
+                pageDir = 'right'
+            else:
+                pageDir = 'left'
+            print(
+                f'#{i+1:>3}: {str(self.pages[i].__class__)[8:-2].split(".")[-1]}({pageDir[:1]})')
+
+            svg = self.pages[i].page[pageDir]
+            svg.name = f'p{i:03}'
+            svg.save(
+                "\\".join([Dir, self.name, 'pages']),
+                width=f'{self.pages[i].svgWidth}mm',
+                height=f'{self.pages[i].svgHeight}mm'
+            )
+            path = "\\".join(['pages', f"{svg.name}.svg"])
+            htmlTxt += f'<img src="{path}">\n'
+
         htmlTxt += '</body>\n</html>'
 
-        with open('\\'.join([Dir, f'{self.name}.html']), "w") as f:
+        with open('\\'.join([Dir, self.name, 'index.html']), "w") as f:
             f.write(htmlTxt)
 
     def toPDF():
