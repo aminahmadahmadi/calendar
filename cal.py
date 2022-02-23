2d3e7a96b601e48e86f1f0f100cb58b308bc4569
aminahamadahmadi <aminahmadahmadi@gmail.com>
2022-02-23 21:29:19 +0330

    rename calendar.py -> cal.py

--- cal.py
+++ cal.py
@@ -1,10 +1,12 @@
 ﻿from pages import *
-from notebook import *
+from notebook import Notebook
 
 
 class Calendar(Notebook):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
+        # general
+        self.name = kwargs.get('name', 'Untitle-Calendar')
 
     def addWeekPage(self):
         page = WeekPage()
