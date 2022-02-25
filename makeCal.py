99dca88faf9ff9ab66d34e1314384772d9d7477c
aminahamadahmadi <aminahmadahmadi@gmail.com>
2022-02-26 01:05:08 +0330

    makeCal.py file to make sample Calendar

---
+++ makeCal.py
@@ -0,0 +1,23 @@
+﻿from unicodedata import name
+from cal import Calendar
+import os
+
+rootPath = os.getcwd()
+
+newBook = Calendar('cal1401.json',
+                   name='cal-1401',
+                   margin=[0, 0, 0, 0],
+                   showWeekdays=True,
+                   secondColor='#f00',
+                   weekend=1,
+                   startWeekday='Sat',
+                   showFullCalendar=False
+                   )
+
+
+newBook.addLinePage()
+for i in range(53):
+    newBook.addLinePage()
+    newBook.addWeekPage(i+1)
+
+newBook.toHTML(rootPath)
