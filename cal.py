18c3c6c7ec283c410fb6e88c6c922f99ef1eb40c
aminahamadahmadi <aminahmadahmadi@gmail.com>
2022-02-25 22:28:02 +0330

    add default font styles

--- cal.py
+++ cal.py
@@ -17,13 +17,44 @@ class Calendar(Notebook):
         self.lineShiftDown = kwargs.get('lineShiftDown', 0)
 
         # Data show
-        self.calendarOrder = kwargs.get('layout', ['sh', 'wc', 'ic'])
+        self.calendarOrder = kwargs.get('calendarOrder', ['sh', 'wc', 'ic'])
         self.showEvents = kwargs.get('showEvents', True)
         self.showWeekdays = kwargs.get('showWeekdays', False)
         self.showFullCalendar = kwargs.get('showFullCalendar', False)
 
         # font style
+        self.fontHeightScl = kwargs.get('fontHeightScl', 0.67)
         self.fontFamily = kwargs.get('fontFamily', 'Anjoman')
+
+        fontWeight = kwargs.get('fontWeight', {})
+        defaultFontWeight = {
+            'firstCal': 'Thin',
+            'holiday': 'Thin',
+            'firstCalWeekdays': 'ExtraLight',
+            'firstCalWeekdaysHoliday': 'ExtraLight',
+            'secondCal': 'Thin',
+            'thirdCal': 'Thin',
+            'monthAndWeek': 'Light',
+            'Events': 'ExtraLight',
+        }
+        self.fontWeight = {}
+        for k, v in defaultFontWeight.items():
+            self.fontWeight[k] = fontWeight.get(k, v)
+
+        fontSize = kwargs.get('fontSize', {})
+        defaultFontSize = {
+            'firstCal':  self.lineHeight * self.daysHeight * 2,
+            'holiday':  self.lineHeight * self.daysHeight * 2,
+            'firstCalWeekdays': 8,
+            'firstCalWeekdaysHoliday': 8,
+            'secondCal': 8,
+            'thirdCal': 8,
+            'monthAndWeek': 8,
+            'Events': 5,
+        }
+        self.fontSize = {}
+        for k, v in defaultFontSize.items():
+            self.fontSize[k] = fontSize.get(k, v)
 
         # colors
         self.primaryColor = kwargs.get('primaryColor', '#000')
@@ -37,6 +68,9 @@ class Calendar(Notebook):
             print('JSON file is not exist.')
             exit()
 
+    def weekKeys(self, i):
+        pass
+
     def addWeekPage(self, i):
-        page = WeekPage(**self.__dict__)
+        page = WeekPage(self.weekKeys(i), **self.__dict__)
         self.pages.append(page)
