7054c2ccaf440b94581027f349a86e617a24aa1f
aminahamadahmadi <aminahmadahmadi@gmail.com>
2022-02-25 22:25:38 +0330

    adding function for transliterate numbers

---
+++ replaceText.py
@@ -0,0 +1,39 @@
+﻿def perNo(txt):
+    txt = str(txt)
+
+    perTxt = [
+        ['0', '۰'],
+        ['1', '۱'],
+        ['2', '۲'],
+        ['3', '۳'],
+        ['4', '۴'],
+        ['5', '۵'],
+        ['6', '۶'],
+        ['7', '۷'],
+        ['8', '۸'],
+        ['9', '۹']
+    ]
+    for a, b in perTxt:
+        txt = txt.replace(a, b)
+    return txt
+
+
+def arbNo(txt):
+    txt = str(txt)
+
+    arbTxt = {
+        ['0', '٠'],
+        ['1', '١'],
+        ['2', '٢'],
+        ['3', '٣'],
+        ['4', '٤'],
+        ['5', '٥'],
+        ['6', '٦'],
+        ['7', '٧'],
+        ['8', '٨'],
+        ['9', '٩']
+    }
+    for a, b in arbTxt:
+        txt = txt.replace(a, b)
+
+    return txt
