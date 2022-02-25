1e5131e76213a02e8d0213160e596039b9ef54b7
aminahamadahmadi <aminahmadahmadi@gmail.com>
2022-02-25 22:56:57 +0330

    WeekPage.addFirstCal() added

--- pages.py
+++ pages.py
@@ -1,4 +1,5 @@
 ﻿from aaaSvg import Svg
+from replaceText import perNo, arbNo
 
 
 class Page():
@@ -156,12 +157,31 @@ class DotPage(Page):
 
 
 class WeekPage(LinePage):
-    def __init__(self, name='Untitle-WeekPage', **kwargs) -> None:
+    def __init__(self, weekKeys, name='Untitle-WeekPage', **kwargs) -> None:
         super().__init__(name, **kwargs)
+
+        self.dataJson = kwargs.get('dataJson', 'left')
+        self.weekKeys = weekKeys
 
         self.layout = kwargs.get('layout', 'left')
         self.daysHeight = kwargs.get('daysHeight', 4)
         self.lineShiftDown = kwargs.get('lineShiftDown', 0)
+
+        # font style
+        self.fontHeightScl = kwargs.get('fontHeightScl', 0.66)
+        self.fontFamily = kwargs.get('fontFamily', 'Anjoman')
+        self.fontWeight = kwargs.get('fontWeight', {})
+        self.fontSize = kwargs.get('fontSize', {})
+
+        # colors
+        self.primaryColor = kwargs.get('primaryColor', '#000')
+        self.secondColor = kwargs.get('secondColor', '#ddd')
+
+        # Data show
+        self.calendarOrder = kwargs.get('calendarOrder', ['sh', 'wc', 'ic'])
+        self.showEvents = kwargs.get('showEvents', True)
+        self.showWeekdays = kwargs.get('showWeekdays', False)
+        self.showFullCalendar = kwargs.get('showFullCalendar', False)
 
     @property
     def page(self):
@@ -191,10 +211,14 @@ class WeekPage(LinePage):
         space = self.lineHeight*self.daysHeight
         xLeftSpace, xRightSpace = self.xloc(loc, space)
 
+        self.daysY = []
         y = self.margin['top'] + self.padding['top']
         lineNo = 0
         while y <= self.svgHeight - self.margin['bottom'] - self.padding['bottom']:
-            if lineNo < self.lineShiftDown or lineNo % self.daysHeight == self.lineShiftDown % self.lineHeight:
+            if lineNo < self.lineShiftDown:
+                xl, xr = xLeft, xRight
+            elif lineNo % self.daysHeight == self.lineShiftDown % self.lineHeight:
+                self.daysY.append(y)
                 xl, xr = xLeft, xRight
             else:
                 if self.layout == 'left':
@@ -211,7 +235,95 @@ class WeekPage(LinePage):
             lineNo += 1
 
     def addFirstCal(self, loc):
-        pass
+        calID = self.calendarOrder[0]
+
+        self.pages[loc].addStyle(
+            'firstCal',
+            f'fill:{self.primaryColor};'
+            f'stroke:None;'
+            f'font-family:"{self.fontFamily} {self.fontWeight.get("firstCal","")}";'
+            f'font-size:{self.fontSize.get("firstCal", self.lineHeight * self.daysHeight * 2)/self.scale}px;'
+            'text-anchor:middle;'
+        )
+        self.pages[loc].addStyle(
+            'holiday',
+            f'fill:{self.secondColor};'
+            f'stroke:None;'
+            f'font-family:"{self.fontFamily} {self.fontWeight.get("holiday","")}";'
+            f'font-size:{self.fontSize.get("holiday", self.lineHeight * self.daysHeight * 2)/self.scale}px;'
+            'text-anchor:middle;'
+        )
+        self.pages[loc].addStyle(
+            'firstCalWeekdays',
+            f'fill:{self.primaryColor};'
+            f'stroke:None;'
+            f'font-family:"{self.fontFamily} {self.fontWeight.get("firstCalWeekdays","")}";'
+            f'font-size:{self.fontSize.get("firstCalWeekdays", 8)/self.scale}px;'
+            'text-anchor:middle;'
+        )
+        self.pages[loc].addStyle(
+            'firstCalWeekdaysHoliday',
+            f'fill:{self.secondColor};'
+            f'stroke:None;'
+            f'font-family:"{self.fontFamily} {self.fontWeight.get("firstCalWeekdaysHoliday","")}";'
+            f'font-size:{self.fontSize.get("firstCalWeekdaysHoliday", 8)/self.scale}px;'
+            'text-anchor:middle;'
+        )
+
+        space = self.lineHeight * self.daysHeight * 0.55
+        xLeftSpace, xRightSpace = self.xloc(loc, space)
+
+        x = xLeftSpace if self.layout == 'left' else xRightSpace
+        for i in range(len(self.weekKeys)):
+            dayKey = self.weekKeys[i]
+
+            # y location
+            dayH = self.fontHeightScl * \
+                self.fontSize.get("firstCal", self.lineHeight *
+                                  self.daysHeight * 2)/self.scale
+            weekdayH = self.fontSize.get("firstCalWeekdays", 8)/self.scale
+
+            if self.showWeekdays:
+                yNo = (self.daysY[i]+self.daysY[i+1])/2 + dayH/2-weekdayH/2-0.5
+                yWeekday = yNo + weekdayH + 1
+            else:
+                yNo = (self.daysY[i]+self.daysY[i+1])/2 + dayH/2
+                yWeekday = 0
+
+            # holiday style
+            holiday = False
+            for e in self.dataJson[dayKey]['event']['values']:
+                if e["dayoff"] == True:
+                    holiday = True
+
+            # text of cal
+            textCal = self.dataJson[dayKey][calID]['date'][2]
+            if calID == 'sh':
+                textCal = perNo(textCal)
+            elif calID == 'ic':
+                textCal = arbNo(textCal)
+
+            textWeekday = self.dataJson[dayKey][calID]['weekday']
+
+            if isinstance(textWeekday, list):
+                textWeekday = textWeekday[1]
+
+            self.pages[loc].addText(
+                x,
+                yNo,
+                textCal,
+                transform=f'scale({self.scale})',
+                class_='holiday' if holiday else 'firstCal'
+            )
+
+            if self.showWeekdays:
+                self.pages[loc].addText(
+                    x,
+                    yWeekday,
+                    textWeekday,
+                    transform=f'scale({self.scale})',
+                    class_='firstCalWeekdaysHoliday' if holiday else 'firstCalWeekdays'
+                )
 
     def addSecondCal(self, loc):
         pass
